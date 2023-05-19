e697a83ac3598af5e293994a3d35217c090bc98c
linli <linli@joinbonnet.com>
2023-05-19 13:13:44 +0100

    update

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import pandas as pd
 # get the base table
 data = {
     'user_id': ['6001ef966c', '600wef966c', '600wef966c', '600we4566c', '600we4566c', '623we4566c', '623we4566c', '623we4536c', '678we4536c', '678we4236c', '678we4236c', '678we4236c'],
-    'date': ['2023-03-01', '2023-03-01', '2023-04-01', '2023-03-01', '2023-05-01', '2023-04-01', '2023-05-01', '2023-04-01', '2023-05-01', '2023-03-01', '2023-04-01', '2023-05-01']}
+    'date': ['2023-03-01', '2023-03-01', '2023-04-01', '2023-03-01', '2023-05-01', '2023-04-01', '2023-05-01', '2023-04-01', '2023-05-01', '2023-03-01', '2023-04-01', '2023-05-01'],
+    'segment': ['LIGHT', 'TURBO', 'LIGHT', 'LIGHT', 'LIGHT', 'LIGHT', 'LIGHT', 'LIGHT', 'TURBO', 'LIGHT', 'TURBO', 'LIGHT']}
 df = pd.DataFrame(data)
 df['date'] = pd.to_datetime(df['date'])
 df['count'] = 1
 
-# Instantiate a cohort object
 cohort = fab_cohort.Cohort()
-result = cohort.count_cohort(df)
+result = cohort.count_cohort_segments(df)
+
+cohort.to_pct(result)
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="fabcohort",
-    version="0.1.0",
+    version="0.1.1",
     description="for cohort analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
